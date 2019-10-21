2ebe2d8c1e7358b8c64657e72b77fbcd2cdc098d
Christoph Hitz <christoph.hitz@fhsstudents.ch>
2019-10-21 11:56:28 +0200

    chatbr

--- easychat/src/app/app.module.ts
+++ easychat/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ChatbarComponent } from './chatbar/chatbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ChatbarComponent
   ],
   imports: [
     BrowserModule
