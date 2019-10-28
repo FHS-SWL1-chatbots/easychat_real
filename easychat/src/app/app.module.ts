844ace80162092ecd4c6983e6091535851d0160e
RadauRuedi <tiziano.pedemonte@fhsstudents.ch>
2019-10-28 11:37:59 +0100

    update1

--- easychat/src/app/app.module.ts
+++ easychat/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ChatbarComponent } from './chatbar/chatbar.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
