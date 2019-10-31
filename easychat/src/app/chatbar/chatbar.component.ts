4fd1ab334b84ffbbaf54d81696db2d86133da926
RadauRuedi <tiziano.pedemonte@fhsstudents.ch>
2019-10-31 12:42:47 +0100

    chattest1

--- easychat/src/app/chatbar/chatbar.component.ts
+++ easychat/src/app/chatbar/chatbar.component.ts
@@ -9,21 +9,27 @@ export class ChatbarComponent implements OnInit {
 
   constructor() { }
 
-  message: string='';
-
-  get usermsg(): string{
-    return this.message;
-  }
+  chatmassage:string = '';
+  postings = '';
+  newline = "\n";
 
   ngOnInit() {
   }
 
+  get usermsg(): string{
+    return this.chatmassage;
+  }
   @Output()
   usermsgchange = new EventEmitter<string>();
+
   @Input ()
   set usermsg(value) {
-    this.message = value;
-    this.usermsgchange.emit(this.message);
+    this.chatmassage = value;
+    this.usermsgchange.emit(this.chatmassage);
+  
+  sendChat() {
+      this.postings=this.postings+this.chatmassage+this.newline;
+      alert('Nachricht erfolgreich versendet! '+this.usermsg);
+      this.usermsg = this.postings;
   }
-
-}
+}}
